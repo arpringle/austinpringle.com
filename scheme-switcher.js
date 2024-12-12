6d85b7ecdad1ec6db5ebbe995727a983212f977c
arpringle <30877107+arpringle@users.noreply.github.com>
2024-12-12 15:45:43 -0500

    Update color-scheme code to use a cookie
    This allows us to persist the color-scheme between pages within the same
    user session, if the user has change it using the button.

--- scheme-switcher.js
+++ scheme-switcher.js
@@ -12,50 +12,63 @@
  *
  */
 
-// Create the color scheme media query, checking for a dark preference
 let color_scheme_query = window.matchMedia("(prefers-color-scheme: dark)");
-
-// Depending on whether the query matches, we set the "mode" variable.
-// false = light mode
-// true = dark mode
 let mode = color_scheme_query.matches;
 
 let site_mode_set = false;
-color_scheme_query.addEventListener("change", function (){
-    if (!site_mode_set) { 
+
+// Read the cookie
+switch (document.cookie) {
+    case "":
+        site_mode_set = false;
+        break;
+    default:
+        site_mode_set = true;
+        let cookies = document.cookie.split("; ");
+        cookies.forEach(cookie => {
+            let [key, value] = cookie.split("=");
+            if (key === "mode") {
+                mode = (value === "true");
+            }
+        });
+}
+
+color_scheme_query.addEventListener("change", function() {
+    if (!site_mode_set) {
         toggleColorScheme();
     }
 });
 
+updateColors();
+
 document.addEventListener("DOMContentLoaded", function() {
-    document.documentElement.className = mode ? "dark-mode" : "light-mode";
 
     let color_scheme_button = document.getElementById("color-scheme-button");
     color_scheme_button.hidden = false;
-    color_scheme_button.addEventListener("click", function(){
+    color_scheme_button.addEventListener("click", function() {
         site_mode_set = true;
         toggleColorScheme();
+        document.cookie = "mode=" + mode + ";";
     });
 
     let color_scheme_button_image = document.getElementById("color-scheme-button-image");
     color_scheme_button_image.src = mode ? "icons/sun-bright.svg" : "icons/moon.svg";
 
-    // When the animation is over, we want to ensure that the style class responsible for animating
-    // it is removed.
+    // Ensure the animation class is removed after the animation ends
     color_scheme_button_image.addEventListener("animationend", function() {
         color_scheme_button_image.classList.remove("animate");
     });
 });
 
-function toggleColorScheme () {
+function toggleColorScheme() {
     mode = !mode;
-    updateColors();
-
     let color_scheme_button_image = document.getElementById("color-scheme-button-image");
     color_scheme_button_image.classList.add("animate");
     color_scheme_button_image.src = mode ? "icons/sun-bright.svg" : "icons/moon.svg";
+    updateColors();
+    document.cookie = "mode=" + mode + ";"; // Update the cookie after toggling
 }
 
-function updateColors () {
+function updateColors() {
     document.documentElement.className = mode ? "dark-mode" : "light-mode";
 }
