702a8659b0864d9ca0dc620087d0f8c9f784e181
arpringle <30877107+arpringle@users.noreply.github.com>
2025-01-21 21:11:37 -0500

    Revert all recent changes
    (for now, we will generate the sites on the local machine)

--- gen/site-gen.py
+++ gen/site-gen.py
@@ -3,35 +3,31 @@ import json
 import textwrap
 from jinja2 import Template
 
-script_dir = os.path.dirname(os.path.realpath(__file__))
-
 # Open the site description file
-file = open(os.path.join(script_dir, "site-desc.json"), "r")
-data = json.load(file)
+site_description_file = open("./site-desc.json", "r")
+site_description = json.load(site_description_file)
 
 # Parse the project tags and add links to the technology-specific pages to index.html
-project_tags = data["project-tags"]
+project_tags = site_description["project-tags"]
 
-skill_grid_template = Template(open(os.path.join(script_dir, "./templates/skill-grid-item.html"), "r").read())
+skill_grid_template = Template(open("./templates/skill-grid-item.html", "r").read())
 
 skill_grid_html_string = ""
 
-index_template = Template(open(os.path.join(script_dir, "./templates/index.html"), "r").read())
+index_template = Template(open("./templates/index.html", "r").read())
 
 for tag in project_tags:
     result = skill_grid_template.render(project_tag=tag["id"],tag_name_nice=tag["name-nice"])
-    skill_grid_html_string = skill_grid_html_string + result
+    skill_grid_html_string = skill_grid_html_string + result + "\n"
 
 skill_grid_html_string = textwrap.indent(skill_grid_html_string, "                    ")
 
-index_html = open(os.path.join(script_dir, "./output/index.html"), "w")
+index_html = open("../index.html", "w")
 index_html.write(index_template.render(skill_grid_items=skill_grid_html_string))
 
 # Create the technology-specific pages
-skill_page_template = Template(open(os.path.join(script_dir, "./templates/skill-page.html"), "r").read())
-
-os.makedirs(os.path.join(script_dir, "./output/projects/"), exist_ok=True)
+skill_page_template = Template(open("./templates/skill-page.html", "r").read())
 
 for tag in project_tags:
-    skill_page_html = open(os.path.join(script_dir, "./output/projects/" + tag["id"] + ".html"), "w")
+    skill_page_html = open("../projects/" + tag["id"] + ".html", "w")
     skill_page_html.write(skill_page_template.render(technology_name=tag["name-nice"]))
