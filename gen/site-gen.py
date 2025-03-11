081ca7969999cfc4a75c7f0e9cf4ff6f818da128
arpringle <git-commits@austinpringle.com>
2025-03-11 11:14:23 -0400

    Automate pages deployment
    Previously, we would have to run the Python script on a local machine
    and then upload the resulting files for deployment. the new CI workflow
    should run the script and build the site automatically, if all goes to
    plan.

--- gen/site-gen.py
+++ gen/site-gen.py
@@ -1,8 +1,21 @@
 import os
 import json
 import textwrap
+import shutil
 from collections import defaultdict
 from jinja2 import Template
+
+# Set the working directory to be this folder (In case the user tries to execute it from a different
+# directory; which would break the relative paths)
+script_dir = os.path.dirname(os.path.abspath(__file__))
+os.chdir(script_dir)
+
+# Clear and remake the public directory
+if os.path.exists("./.public"):
+    shutil.rmtree("./.public")
+os.mkdir("./.public")
+os.mkdir("./.public/projects")
+
 
 # Open the site description file
 site_description_file = open("./site-desc.json", "r")
@@ -27,7 +40,7 @@ for tag in project_tags:
 
 skill_grid_html_string = textwrap.indent(skill_grid_html_string, "                    ")
 
-index_html = open("../index.html", "w")
+index_html = open("./.public/index.html", "w")
 index_html.write(index_template.render(skill_grid_items=skill_grid_html_string))
 
 #
@@ -42,17 +55,17 @@ projects = site_description["projects"]
 project_tiles_concatenated = ""
 
 for project in projects:
-    project_tile_rendered = project_tile_template.render(project_id=project["id"], project_name=project["name-nice"], project_description=project["description"])
+    project_tile_rendered = project_tile_template.render(project_id=project["id"], project_name=project["name-nice"])
     for tag in project["tags"]:
         tags_to_project_render_dict[tag].append(project_tile_rendered)
     project_tiles_concatenated += project_tile_rendered
 
-project_tiles_concatenated= textwrap.indent(project_tiles_concatenated, "                ")
+project_tiles_concatenated = textwrap.indent(project_tiles_concatenated, "                ")
 
 projects_index_template = Template(open("./templates/projects-index.html", "r").read())
 projects_index_rendered = projects_index_template.render(projects=project_tiles_concatenated)
 
-projects_index_html = open("../projects/index.html", "w")
+projects_index_html = open("./.public/projects/index.html", "w")
 projects_index_html.write(projects_index_rendered)
 
 #
@@ -61,5 +74,16 @@ projects_index_html.write(projects_index_rendered)
 skill_page_template = Template(open("./templates/skill-page.html", "r").read())
 
 for tag in project_tags:
-    skill_page_html = open("../projects/" + tag["id"] + ".html", "w")
+    skill_page_html = open("./.public/projects/" + tag["id"] + ".html", "w")
     skill_page_html.write(skill_page_template.render(technology_id=tag["id"], technology_name=tag["name-nice"]))
+
+shutil.copytree("../icons", "./.public/icons");
+shutil.copytree("../pics", "./.public/pics");
+shutil.copytree("../project-screenshots", "./.public/projects/project-screenshots");
+shutil.copytree("../scripts", "./.public/scripts");
+shutil.copytree("../styles", "./.public/styles");
+
+shutil.copy("../favicon.ico", "./.public/favicon.ico")
+
+#TODO
+shutil.copy("./templates/blog.html", "./.public/blog.html")
